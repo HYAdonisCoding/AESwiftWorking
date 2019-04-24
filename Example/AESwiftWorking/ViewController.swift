81fc0bb092f964c0b3e5cf003befff9b4deb8fe8
曹弘扬 <296786475@qq.com>
2019-04-24 17:27:13 +0800

    Initial commit

---
+++ Example/AESwiftWorking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AESwiftWorking
+//
+//  Created by HYAdonisCoding on 04/24/2019.
+//  Copyright (c) 2019 HYAdonisCoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
