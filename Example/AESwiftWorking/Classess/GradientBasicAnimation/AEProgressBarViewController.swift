fa7b3d3cd26b95d9f023dbbe18a1174958d3d62b
曹弘扬 <>
2021-10-28 14:31:16 +0800

    add AEProgressView

--- Example/AESwiftWorking/Classess/GradientBasicAnimation/AEProgressBarViewController.swift
+++ Example/AESwiftWorking/Classess/GradientBasicAnimation/AEProgressBarViewController.swift
@@ -9,63 +9,164 @@
 import UIKit
 
 class AEProgressBarViewController: BaseViewController {
-    let shapeLayer = CAShapeLayer()
+    
+    var shapeLayer = CAShapeLayer()
+    let urlString = "https://pics7.baidu.com/feed/37d12f2eb9389b5038d39bfbeef5b1d4e6116eb0.jpeg?token=f63d57e095f197046906ccd4a63d51e2"
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .black
+        return label
+    }()
+    
+    var downloadTask: URLSessionDownloadTask?
+    
+    var pulsatingLayer = CAShapeLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     override func configEvent() {
         super.configEvent()
         
-        shapeLayer.strokeEnd = 0
     }
     override func configUI() {
         super.configUI()
         
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
-        let trackLayer = CAShapeLayer()
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.position = view.center
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineCap = .round
-        view.layer.addSublayer(trackLayer)
-        
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.position = view.center
-        
-        view.layer.addSublayer(shapeLayer)
-        
-        
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 10
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        
-        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
-        shapeLayer.lineCap = .round
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        
-        
+        view.backgroundColor = UIColor.backgroundColor
         
 
-
+        let y = 100
+        for item in 0...5 {
+            let progressBar = AEProgressView(frame: CGRect(x: 100, y: y*(1 + item), width: 80, height: 80))
+            self.view.addSubview(progressBar)
+        }
+//        setupNotificationObservers()
+//
+//        view.backgroundColor = UIColor.backgroundColor
+//
+//        setupCircleLayers()
+//
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+//
+//        setupPercentageLabel()
     }
-
-
+    
+    
     // MARK: - Navigation
-
-
+    
+    
 }
 
 extension AEProgressBarViewController {
-    
-    @objc func handleTap() {
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = 1
-        basicAnimation.duration = 2
-        shapeLayer.add(basicAnimation, forKey: "stokeAnimation")
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
+    
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = .round
+        layer.position = view.center
+        return layer
+    }
+    
+    private func setupPercentageLabel() {
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+    }
+    
+    private func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulsatingFillColor)
+        view.layer.addSublayer(pulsatingLayer)
+        animatePulsatingLayer()
+        
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+        view.layer.addSublayer(trackLayer)
+        
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
+        
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.5
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    @objc private func handleEnterForeground() {
+        animatePulsatingLayer()
+    }
+    @objc func handleTap() {
+//        beginDownloadingFile()
+                let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+                basicAnimation.toValue = 1
+                basicAnimation.duration = 2
+                basicAnimation.fillMode = .forwards
+                basicAnimation.isRemovedOnCompletion = false
+                shapeLayer.add(basicAnimation, forKey: "stokeAnimation")
+    }
+    
+    
+    func beginDownloadingFile() {
+        let configuration = URLSessionConfiguration.default
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
+        
+        guard let url = URL(string: urlString) else { return }
+        downloadTask = urlSession.downloadTask(with: url)
+        downloadTask?.resume()
+    }
+}
+
+extension AEProgressBarViewController: URLSessionDelegate {
+    
+    
+    
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+         let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+
+         DispatchQueue.main.async {
+             self.percentageLabel.text = "\(Int(percentage * 100))%"
+             self.shapeLayer.strokeEnd = percentage
+         }
+
+         print(percentage)
+     }
+}
+
+extension UIColor {
+
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+
+    static let backgroundColor = UIColor.rgb(r: 21, g: 22, b: 33)
+    static let outlineStrokeColor = UIColor.rgb(r: 234, g: 46, b: 111)
+    static let trackStrokeColor = UIColor.rgb(r: 56, g: 25, b: 49)
+    static let pulsatingFillColor = UIColor.rgb(r: 86, g: 30, b: 63)
 }
---
+++ Example/AESwiftWorking/Classess/GradientBasicAnimation/AEProgressView.swift
@@ -0,0 +1,115 @@
+//
+//  AEProgressView.swift
+//  AESwiftWorking_Example
+//
+//  Created by Adam on 2021/10/28.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class AEProgressView: AEBaseView {
+    var shapeLayer = CAShapeLayer()
+    
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = .white
+        label.isUserInteractionEnabled = true
+        return label
+    }()
+    
+    
+    var pulsatingLayer = CAShapeLayer()
+    
+
+}
+
+extension AEProgressView {
+    override func configEvent() {
+        super.configEvent()
+        
+    }
+    
+    override func configUI() {
+        super.configUI()
+        
+//        setupNotificationObservers()
+        
+        self.backgroundColor = UIColor.backgroundColor
+        
+        setupCircleLayers()
+        
+        percentageLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        setupPercentageLabel()
+    }
+    
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: self.frame.size.width*0.45, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20*self.frame.size.width/UIScreen.main.bounds.size.width
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = .round
+        layer.position = self.center
+        return layer
+    }
+    
+    private func setupPercentageLabel() {
+        self.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width*0.8, height: self.frame.size.width*0.8)
+        percentageLabel.center = self.center
+        
+        
+    }
+    
+    private func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulsatingFillColor)
+        self.layer.addSublayer(pulsatingLayer)
+        animatePulsatingLayer()
+        
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+        self.layer.addSublayer(trackLayer)
+        
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.4
+        animation.duration = 0.9
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    @objc private func handleEnterForeground() {
+        animatePulsatingLayer()
+    }
+    @objc func handleTap() {
+        //        beginDownloadingFile()
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 5
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimation, forKey: "stokeAnimation")
+    }
+}
+
